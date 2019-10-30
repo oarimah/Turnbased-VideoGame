2afb25581932686fc30dcbc339d1ecfdfe739034
jakenemiroff <jnemirof@uwo.ca>
2019-10-29 21:51:00 -0400

    add methods for Tile class, test with main with 'testTile.png'

--- tile.h
+++ tile.h
@@ -1,28 +1,32 @@
 #ifndef TILE_H
 #define TILE_H
 
-//will need to know which unit is occupying a given tile, so will need to include the unit.h file
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include "ImageHandler.h"
+#include <iostream>
+#include "GameObject.h"
 
-//will also need to know about the graphics to determine what to render on each tile
+class Tile: public GameObject{
 
+private:
+	SDL_Rect position;
+	SDL_Texture* image;
+	bool clicked;
+	ImageHandler* imageHandler;
 
-class Tile {
-
-	public:
-		Tile(int x, int y);
-
-		~Tile();
-
-		void clearTile(Tile tile);
-
-
-
-	private:
-		int positionX, positionY;
-		bool isOccupied;
-
-
-}
+public:
+	Tile(int xPos, int yPos, int height, int width, const std::string& imageFile, ImageHandler* imgHandler);
+	~Tile();
+	const SDL_Rect* getPosition();
+	void changePosition(int newX, int newY);
+	const bool isClicked();
+	void changeImage(const std::string& imageFile);
+	void render();
+	const std::string getType();
+	void changeClicked(bool value);
+};
 
 
 #endif
