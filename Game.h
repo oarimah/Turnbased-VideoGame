bc6c495ca91facba1ebbd37a4f87b12d82a813c1
Carolyn Owen <cowen23@uwo.ca>
2019-10-31 17:52:34 -0400

    Game, Game.h and Main.cpp modified to get closer to end target

--- Game.h
+++ Game.h
@@ -4,31 +4,32 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
-//#include "Player.h"
+#include "Player.h"
 #include "TextDisplay.h"
-//#include "Map.h"
+#include "backgroundMap.h"
+#include "foregroundMap.h"
 #include <iostream>
 
 
-//define static variables for game class's tile height and width
-static const int TILE_HEIGHT = 32;
-static const int TILE_WIDTH = 32;
 
 
 class Game {
 private: 
-
-	//Player* players[];
-	//Map* backgroundMap, foregroundMap;
+	
+	Player* players[2];
+	int currentPlayerIndex;
+	backgroundMap* backgroundMap;
+	foregroundMap* foregroundMap;
 	bool isRunning;
-	int height, width;
+	int height, width, tileHeight, tileWidth;
 	ImageHandler* imageHandler;
 	SDL_Renderer* renderer;
 	TextDisplay* displayBox;
+	Button* continueButton;
 
 public:
 
-	Game(int numTilesHigh, int numTilesWide);
+	Game(int numTilesHigh, int numTilesWide, int tileWidth, int tileHeight);
 	~Game();
 	int init();
 	void eventHandler(const SDL_Event* event);
