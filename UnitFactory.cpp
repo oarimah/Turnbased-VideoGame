8c1d0b06e2214eb637ecd0366a6e0e3bc4dfffb3
Abdallah <aalasmar@uwo.ca>
2019-11-22 19:17:20 -0500

    added new unit information into UnitFactory

--- UnitFactory.cpp
+++ UnitFactory.cpp
@@ -38,8 +38,37 @@ Unit *UnitFactory::createUnit(int xPos, int yPos, int height, int width,
 						"Characters/" + colour + "LightKnight.png", imgHandler, 55, 20, 5,
 						2, 1, 1, 4);
 			}
-		}
-	
+		} else if (faction == 3) { // the germans
+                        if (unitType == 0) { // teutonic knight
+                                return new Unit(xPos, yPos, height, width,
+                                                "Characters/" + colour + "General.png", imgHandler, 120, 20, 5, 2,
+                                                1, 1, 2);
+                        } else if (unitType == 1) { // cuyler
+                                return new Unit(xPos, yPos, height, width,
+                                                "Characters/" + colour + "Bandit.png", imgHandler, 80, 14, 3, 3,
+                                                2, 3, 2);
+                        } else if (unitType == 2) { // turcopole
+                                return new Unit(xPos, yPos, height, width,
+                                                "Characters/" + colour + "Knight.png", imgHandler, 70, 10, 3,
+                                                4, 2, 3, 3);
+                        }
+                } else if (faction == 4) { // the spanish
+                        if (unitType == 0) { // conquistador
+                                return new Unit(xPos, yPos, height, width,
+                                                "Characters/" + colour + "Infantry.png", imgHandler, 65, 12, 6, 3,
+                                                2, 2, 4);
+                        } else if (unitType == 1) { // arquebusier
+                                return new Unit(xPos, yPos, height, width,
+                                                "Characters/" + colour + "Wrapman.png", imgHandler, 30, 23, 1, 2,
+                                                2, 3, 4);
+                        } else if (unitType == 2) { // jinete
+                                return new Unit(xPos, yPos, height, width,
+                                                "Characters/" + colour + "LightKnight.png", imgHandler, 60, 30, 3,
+                                                2, 1, 1, 6);
+                        }
+                }
+
+
 	return NULL;
 }
 
